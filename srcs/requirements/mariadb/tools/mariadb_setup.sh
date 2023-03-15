e8ad5f47ef0c235082f2207446431ad71e0f0120
deanioche <briochefavori@gmail.com>
2023-03-16 06:51:03 +0900

    fix sh files

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -2,26 +2,26 @@
 
 set -e
 
-MARIADB_SETUP_FILE=/var/lib/mysql/.setup
+MYSQL_SETUP_FILE=/var/lib/mysql/.setup
 
 service mysql start
 
-if [ ! -e $MARIADB_SETUP_FILE ]; then # 처음에만 실행되도록 하기 위해서
+if [ ! -e $MYSQL_SETUP_FILE ]; then # 처음에만 실행되도록 하기 위해서
 
 	# mysql -e : mysql 에서 명령을 실행하라는 의미
 	
-	mysql -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE";
-	mysql -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD'";
-	mysql -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%'";
+	mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
+	mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
+	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
 	# 변경사항을 적용하라는 의미
 	mysql -e "FLUSH PRIVILEGES";
 	# root 계정의 비밀번호를 변경한다.
-	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'";
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
 
-	# mysql $MARIADB_DATABASE -uroot -p$MARIADB_ROOT_PASSWORD
-	mysqladmin -uroot -p$MARIADB_ROOT_PASSWORD shutdown
+	# mysql $MYSQL_DATABASE -uroot -p$MYSQL_ROOT_PASSWORD
+	mysqladmin -uroot -p$MYSQL_ROOT_PASSWORD shutdown
 
-	touch $MARIADB_SETUP_FILE
+	touch $MYSQL_SETUP_FILE
 fi
 
 exec mysqld_safe
